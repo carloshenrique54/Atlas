df3797c8312d9c7dba04e15589da2c4e180e3f4f
CARLOS HENRIQUE MORAES DOS SANTOS <carlos.santos14@portalsesisp.org.br>
2026-05-22 16:08:16 -0300

    Agora os links do header ficam 'Ativos' dependendo da localização da tela

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,43 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 function Header() {
   const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [scrollY, setScrollY] = useState(0)
+  const [posicaoTela, setProsicaoTela] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY)
+    };
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll);
+  },[])
+
+  useEffect(() => {
+
+    switch (true){
+    case scrollY > 3700:
+      setProsicaoTela("contato")
+      break
+    case scrollY > 3000:
+      setProsicaoTela("sobre")
+      break
+    case scrollY > 2100:
+      setProsicaoTela("planos")
+      break
+    case scrollY > 1300:
+      setProsicaoTela("beneficios")
+      break
+    case scrollY > 600:
+      setProsicaoTela("funcionalidades")
+      break
+    default:
+      setProsicaoTela("inicio")
+      break
+    }
+  }, [scrollY])
+
+  console.log(scrollY)
 
   useEffect(() => {
     const handleScroll = () => setScrolled(window.scrollY > 20);
@@ -24,12 +61,12 @@ function Header() {
           </a>
 
           <nav className="header-nav">
-            <a href="/#inicio">Início</a>
-            <a href="/#funcionalidades">Funcionalidades</a>
-            <a href="/#beneficios">Benefícios</a>
-            <a href="/#planos">Planos</a>
-            <a href="/#sobre">Sobre</a>
-            <a href="/#contato">Contato</a>
+            <a href="/#inicio" className={posicaoTela === "inicio" ? "ativo" : null } >Início</a>
+            <a href="/#funcionalidades" className={posicaoTela === "funcionalidades" ? "ativo" : null } >Funcionalidades</a>
+            <a href="/#beneficios" className={posicaoTela === "beneficios" ? "ativo" : null } >Benefícios</a>
+            <a href="/#planos" className={posicaoTela === "planos" ? "ativo" : null } >Planos</a>
+            <a href="/#sobre" className={posicaoTela === "sobre" ? "ativo" : null } >Sobre</a>
+            <a href="/#contato" className={posicaoTela === "contato" ? "ativo" : null } >Contato</a>
           </nav>
 
           <nav className="header-actions">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faDesktop, faCircleCheck, faBuilding, faUser,
